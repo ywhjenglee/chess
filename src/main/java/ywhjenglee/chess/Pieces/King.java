45d73e4a1813b31aa9af96df269847cbf64edc2b
Ywh-Jeng Lee <ywhjeng@gmail.com>
2020-12-09 18:19:46 -0500

    Implemented generatePossibleMoves for King

--- src/main/java/ywhjenglee/chess/Pieces/King.java
+++ src/main/java/ywhjenglee/chess/Pieces/King.java
@@ -9,6 +9,17 @@ public class King extends Piece {
     }
 
     public void generatePossibleMoves(Tile[][] pChessBoard, Tile pTile){
-        
+        int x = pTile.getTileX();
+        int y = pTile.getTileY();
+        legalMoves[x][y] = true;
+        legalMoves[x+1][y] = true;
+        legalMoves[x-1][y] = true;
+        legalMoves[x][y+1] = true;
+        legalMoves[x][y-1] = true;
+        legalMoves[x+1][y+1] = true;
+        legalMoves[x-1][y+1] = true;
+        legalMoves[x+1][y-1] = true;
+        legalMoves[x-1][y-1] = true;
+        removeOutOfBounds();
     }
 }
